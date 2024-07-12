d08ff3bb0b3fb63a20ac720934c47de73fd46ce4
Melody Ziaee <melody.ziaee@gmail.com>
2024-07-12 16:03:44 -0400

    Initial Commit

---
+++ Final Project!/Final_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_App.swift
+//  Final Project!
+//
+//  Created by Melody Ziaee on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
